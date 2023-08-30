2333c2a23cda0f5109df27e716cda0f65e4d2421
Saurabh Patil <sourabh.maualtesting@gmail.com>
2023-08-30 02:22:18 -0700

    added main.py

---
+++ FastAPI/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+user = {1:{"name":"sourabh","language":"python"}}
+@app.get("/{id}")
+def root(id: int):
+    data = user[id]
+    print(data)
+    return data
+
+@app.post("/{id}")
+def root(id: int, data: dict):
+    user[id]=data
+    print(user)
+    return data
+
+
+@app.put("/{id}")
+def root(id: int, data: dict):
+    user[id]=data
+    print(user)
+    return data
